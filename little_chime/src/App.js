376458d167b4170c87859dbfdb501491c9ddfd80
Vidhi Sharma <vidhisharma@Vidhis-MacBook-Air.local>
2023-02-23 11:24:20 +0530

    day2

--- little_chime/src/App.js
+++ little_chime/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { AddProducts } from './pages/AddProducts';
 import Navbar from './components/Navbar';
+import Header from "./components/Header"
+import SimpleSlider from "./components/Carousel";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-     {/* <AddProducts/> */}
-     <Navbar/>
+      {/* <AddProducts/> */}
+      <Header />
+      <Navbar />
+      <SimpleSlider />
+      <Footer/>
     </div>
   );
 }
